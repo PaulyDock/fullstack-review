4872a88ee3adb84271b342d4dc3fdfa216d04bfd
Hack Reactor Student <student@hackreactor.com>
2017-06-16 14:19:41 -0700

    impl placeholder schema for mongoDB

--- database/index.js
+++ database/index.js
@@ -2,7 +2,17 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 var repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  full_name: String,
+  owner: {
+    login: String,
+    id: Number,
+    url: String,
+    repos_url: String,
+  },
+  updated_at: Date,
+  default_branch: String
 });
 
 var Repo = mongoose.model('Repo', repoSchema);
